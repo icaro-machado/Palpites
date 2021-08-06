257e286fa0eda064b2a894d5539c49e6b11e7d73
Icaro Machado <icarodesousamachado@gmail.com>
2021-08-06 07:54:08 -0300

    atualização dos arquivos

---
+++ salvar.php
@@ -0,0 +1,9 @@
+<?php
+
+$nome = $_GET['name'];
+$idade = $_GET['idade'];
+$chute = $_GET['chute'];
+
+echo "Olá ".$nome."!";
+echo "Sua idade é ".$idade."!";
+echo "O número escolhido por você foi: ".$chute;
