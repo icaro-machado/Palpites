8f918bbce44fc86d8cba0186882993a1ca096867
Icaro Machado <icarodesousamachado@gmail.com>
2021-08-07 14:22:37 -0300

    atualizacao dos arquivos

--- salvar.php
+++ salvar.php
@@ -4,6 +4,18 @@ $nome = $_GET['name'];
 $idade = $_GET['idade'];
 $chute = $_GET['chute'];
 
-echo "Olá ".$nome."!";
-echo "Sua idade é ".$idade."!";
-echo "O número escolhido por você foi: ".$chute;
+$sorteio = rand(1,10);
+
+echo "Olá ".$nome."!<br>";
+echo "Sua idade é ".$idade."!<br>";
+echo "O número escolhido por você foi: ".$chute."<br>";
+echo "O número sorteado foi ".$sorteio."<br><br>";
+
+if ($chute == $sorteio) {
+    echo "ACERTOU-MIZERAVI!!<br>";
+    echo "<img src='https://25horasdehumor.files.wordpress.com/2015/09/acertou-mizeravi.jpg'>";
+} else {
+    echo "ERROU-MIZERAVI!!<br>";
+    echo "<img src='https://conteudo.imguol.com.br/c/bol/fotos/97/2018/01/10/faustao-mostrou-que-nem-sempre-quem-sabe-faz-ao-vivo-1515582826765_615x300.jpg'>";
+}
+
